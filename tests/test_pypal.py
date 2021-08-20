fe07a1f2c6c310706e2f25c19262d1aa8a3d47a9
byteface <byteface@googlemail.com>
2021-08-20 09:19:20 +0100

    start a tests folder

---
+++ tests/test_pypal.py
@@ -0,0 +1,20 @@
+"""
+    test_pypal
+    ~~~~~~~~~~~~
+    unit tests for pypal
+"""
+
+import unittest
+# import json
+# import requests
+# from mock import patch
+
+# from . import PyPal
+
+class TestCase(unittest.TestCase):
+
+    def test_pypal(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
